2078bcd435c6009a8b6be745bed8509315f402ae
Haresh Gayakhe <hgayakhe7@gmail.com>
2024-05-17 15:21:42 +0530

    completed

--- RollTheDice/App.tsx
+++ RollTheDice/App.tsx
@@ -1,118 +1,100 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
-import React from 'react';
-import type {PropsWithChildren} from 'react';
 import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
+  Image,
+  ImageSourcePropType,
   StyleSheet,
   Text,
-  useColorScheme,
+  TouchableOpacity,
   View,
 } from 'react-native';
+import React, {PropsWithChildren, useState} from 'react';
+import RNReactNativeHapticFeedback from 'react-native-haptic-feedback'; //package for haptic feedback
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import One from './assets/One.png';
+import Two from './assets/Two.png';
+import Three from './assets/Three.png';
+import Four from './assets/Four.png';
+import Five from './assets/Five.png';
+import Six from './assets/Six.png';
 
-type SectionProps = PropsWithChildren<{
-  title: string;
+type DiceProps = PropsWithChildren<{
+  imageUrl: ImageSourcePropType;
 }>;
 
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+const Dice = ({imageUrl}: DiceProps) => {
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
+    <View>
+      <Image source={imageUrl} style={styles.image} />
     </View>
   );
-}
+};
 
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+const App = () => {
+  const [image, setImage] = useState<ImageSourcePropType>(One);
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  const changeImage = () => {
+
+    //haptic feedback trrigres here
+    RNReactNativeHapticFeedback.trigger('effectDoubleClick');
+
+    let image = Math.round(Math.random() * 6);
+    switch (image) {
+      case 1:
+        setImage(One);
+        break;
+      case 2:
+        setImage(Two);
+        break;
+      case 3:
+        setImage(Three);
+        break;
+      case 4:
+        setImage(Four);
+        break;
+      case 5:
+        setImage(Five);
+        break;
+      case 6:
+        setImage(Six);
+        break;
+      default:
+        break;
+    }
   };
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+    <View style={styles.container}>
+      <Dice imageUrl={image} />
+      <TouchableOpacity style={styles.btn} onPress={changeImage}>
+        <Text style={styles.btnText}>Roll Dice</Text>
+      </TouchableOpacity>
+    </View>
   );
-}
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
+};
 
 export default App;
+
+const styles = StyleSheet.create({
+  image: {
+    width: 200,
+    height: 200,
+  },
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  btn: {
+    backgroundColor: 'purple',
+    width: 200,
+    height: 50,
+    borderRadius: 10,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 15,
+  },
+  btnText: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: 'white',
+  },
+});
